ee5f98ed4080909813f0a0f8c2182daba75c2d6e
NandiniC-54 <nandinichanagari54@gmail.com>
2026-03-22 15:58:07 +0530

    Deployment: Support environment variables for Firebase Admin on Render

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -2,8 +2,20 @@ const admin = require('firebase-admin');
 
 let db;
 try {
-  const serviceAccount = require('../../serviceAccountKey.json');
+  let serviceAccount;
   
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else if (process.env.FIREBASE_PRIVATE_KEY) {
+    serviceAccount = {
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    };
+  } else {
+    serviceAccount = require('../../serviceAccountKey.json');
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
@@ -11,7 +23,7 @@ try {
   db = admin.firestore();
   console.log('Firebase Admin initialized successfully!');
 } catch (error) {
-  console.error('Firebase Admin initialization error. Make sure you pasted your keys into serviceAccountKey.json!', error.message);
+  console.error('Firebase Admin initialization error:', error.message);
 }
 
 module.exports = { admin, db };
